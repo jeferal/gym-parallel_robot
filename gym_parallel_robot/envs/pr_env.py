5b319dd8863490b50a0103ac07237b029465fad2
Jesús Ferrándiz <jeferrandiz98@gmail.com>
2021-02-15 16:29:01 +0100

    filled functions

--- gym_parallel_robot/envs/pr_env.py
+++ gym_parallel_robot/envs/pr_env.py
@@ -10,22 +10,25 @@ from rclpy.node import Node
 from pr_msgs.msg import PRState
 from pr_msgs.msg import PRArrayH
 
-class PREnv(gym.Env, Node):
+class PREnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        super().__init__('pr_env_node')
         print("Env initialized")
 
+        #create ROS2 node
+        self.node = rclpy.create_node("pr_env")
+
         #initialize variables
-        self.obs = 0
+        self.obs = np.array([])
+        self.target_position = np.array([0.6, 0.6, 0.6, 0.6])
 
         #create ROS2 communication
-        self.publisher_ = self.create_publisher(
+        self.publisher_ = self.node.create_publisher(
             PRArrayH, 
             'control_action', 
             1)
-        self.subscription_ = self.create_subscription(
+        self.subscription_ = self.node.create_subscription(
             PRState, 
             'pr_state',
             self._state_callback, 
@@ -39,7 +42,7 @@ class PREnv(gym.Env, Node):
         obs_ = self._get_obs()
 
         #calculate reward
-        reward = self._calculate_reward()
+        reward = self._calculate_reward(self.target_position, obs_)
 
         #check if done
         done = False
@@ -49,37 +52,41 @@ class PREnv(gym.Env, Node):
             "pr": 'parallel_robot'
             }
 
-        print("Step Success")
-
         return (obs_, reward, done, info)
 
     def reset(self):
+        obs = self._get_obs()
         print("Env reset")
+        return obs
 
     def render(self):
         print("Env rendered")
 
     def close(self):
         print("Env closed")
-        self.destroy_node()
+        self.node.destroy_node()
         rclpy.shutdown()
 
     def _state_callback(self, msg):
         #Update obs
         self.obs = np.array([msg.q.data, msg.q_vel.data])
-        print("State cb")
 
     def _set_action(self, action):
         msg = PRArrayH()
-        msg.current_time = self.get_clock().now()
+        msg.current_time = self.node.get_clock().now().to_msg()
         msg.data = action
         
         self.publisher_.publish(msg)
 
     def _get_obs(self):
+        """
+        Take observation from the environment and return it
+        """
+        rclpy.spin_once(self.node)
+        #Check that the observation is not prior to the action TODO
         return self.obs
 
-    def _calculate_reward(self):
-        print("calculating reward")
-        reward = 0
+    def _calculate_reward(self, target, pos):
+        reward = - np.square(np.subtract(target, pos)).sum()
+        print(reward)
         return reward
