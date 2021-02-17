29572e45667b9e77edebdba295c9f1ae79de0cea
Jesús Ferrándiz <jeferrandiz98@gmail.com>
2021-02-17 14:13:14 +0100

    changed matlab interface

--- gym_parallel_robot/envs/pr_env.py
+++ gym_parallel_robot/envs/pr_env.py
@@ -6,9 +6,14 @@ import numpy as np
 
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy, QoSDurabilityPolicy
 
 from pr_msgs.msg import PRState
 from pr_msgs.msg import PRArrayH
+from geometry_msgs.msg import Quaternion
+from std_msgs.msg import Bool, String
+
+from time import sleep
 
 class PREnv(gym.Env):
     metadata = {'render.modes': ['human']}
@@ -17,11 +22,16 @@ class PREnv(gym.Env):
         print("Env initialized")
 
         #create ROS2 node
+        rclpy.init(args=None)
         self.node = rclpy.create_node("pr_env")
 
         #initialize variables
-        self.obs = np.array([])
-        self.target_position = np.array([0.6, 0.6, 0.6, 0.6])
+        self.obs = np.array([[0.695245, 0.691370, 0.693736, 0.654154],
+                            [0.0, 0.0, 0.0, 0.0]])
+        self.target_position = np.array([0.8, 0.8, 0.8, 0.8])
+        self.status = "starting"
+        self.time_obs_ns = 0
+        self.time_obs_ns_ = self.time_obs_ns
 
         #create ROS2 communication
         self.publisher_ = self.node.create_publisher(
@@ -34,6 +44,31 @@ class PREnv(gym.Env):
             self._state_callback, 
             1)
 
+        sim_qos = QoSProfile(depth=1)
+        sim_qos.reliability = QoSReliabilityPolicy.BEST_EFFORT
+        sim_qos.history = QoSHistoryPolicy.KEEP_LAST
+        sim_qos.durability = QoSDurabilityPolicy.VOLATILE
+
+        self.subscription_sim_ = self.node.create_subscription(
+            Quaternion,
+            "posicion_sim",
+            self._sim_callback,
+            sim_qos)
+
+        #create pub and sub for matlab macro
+        self.publisher_start_ = self.node.create_publisher(
+            Bool,
+            "start_flag", 
+            1)
+
+        self.subscription_end_ = self.node.create_subscription(
+            String,
+            "status_sim",
+            self._end_callback,
+            1)
+
+        start = input('Press any key to start')
+
     def step(self, action):
         #set action
         self._set_action(action)
@@ -45,7 +80,11 @@ class PREnv(gym.Env):
         reward = self._calculate_reward(self.target_position, obs_)
 
         #check if done
-        done = False
+        if self.status == 'all_clear' or self.status == 'error_sim':
+            done = True
+            print('DONEEEEEEE')
+        else:
+            done = False
 
         #create info dic
         info = {
@@ -55,9 +94,25 @@ class PREnv(gym.Env):
         return (obs_, reward, done, info)
 
     def reset(self):
-        obs = self._get_obs()
-        print("Env reset")
-        return obs
+        #start simulink thread
+        self.status = "waiting for simulation"
+
+        #send signal and wait for response
+        while self.status != 'starting_simulation':
+            msg = Bool()
+            msg.data = True
+            rclpy.spin_once(self.node, timeout_sec=1)
+            self.publisher_start_.publish(msg)
+
+        #wait for simulink
+        print('waiting for simulink')
+        while self.status != 'running':
+            rclpy.spin_once(self.node, timeout_sec=1)
+
+        print("simulation started")
+        self.obs = np.array([[0.695245, 0.691370, 0.693736, 0.654154],
+                            [0.0, 0.0, 0.0, 0.0]])
+        return self.obs
 
     def render(self):
         print("Env rendered")
@@ -70,6 +125,7 @@ class PREnv(gym.Env):
     def _state_callback(self, msg):
         #Update obs
         self.obs = np.array([msg.q.data, msg.q_vel.data])
+        self.time_obs_ns = msg.current_time.nanosec
 
     def _set_action(self, action):
         msg = PRArrayH()
@@ -82,11 +138,26 @@ class PREnv(gym.Env):
         """
         Take observation from the environment and return it
         """
-        rclpy.spin_once(self.node)
-        #Check that the observation is not prior to the action TODO
+        #Sping until a new observation (with a different time stamp)
+        while self.time_obs_ns == self.time_obs_ns_:
+            rclpy.spin_once(self.node)
+            print(self.time_obs_ns)
+            print(self.time_obs_ns_)
+            if self.status == 'all_clear' or self.status == 'error_sim':
+                break
+
+        self.time_obs_ns_ = self.time_obs_ns
         return self.obs
 
     def _calculate_reward(self, target, pos):
         reward = - np.square(np.subtract(target, pos)).sum()
         print(reward)
         return reward
+    
+    def _sim_callback(self, msg):
+        print('sim callback')
+        self.status = "running"
+
+    def _end_callback(self, msg):
+        self.status = msg.data
+        print(self.status)
